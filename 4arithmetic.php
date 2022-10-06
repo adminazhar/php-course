08f6ed886be49b7b44460dcd17e832383905bc92
Azhar Khan <admin@azhark.com>
2022-10-06 14:09:31 +0530

    Lecture 4

---
+++ 4arithmetic.php
@@ -0,0 +1,27 @@
+<?php
+
+// Sum
+echo 5+10;
+echo "<br>";
+
+// Minus
+echo 10-5;
+echo "<br>";
+
+// Divide
+echo 9/3;
+echo "<br>";
+
+// Multiply
+echo 9*5;
+echo "<br>";
+
+
+// Modulus
+echo 10%2;
+echo "<br>";
+
+// Exponential
+echo 5**5;
+
+?>
