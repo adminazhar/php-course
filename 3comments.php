abb3dd4c47f7951233e5475e1822ef642900a8c8
Azhar Khan <admin@azhark.com>
2022-10-06 14:09:04 +0530

    Lecture 3

---
+++ 3comments.php
@@ -0,0 +1,17 @@
+<?php
+//echo "Hello World";
+//echo "Gotham Coding";
+
+// Netflix
+echo "Naruto is best";
+
+/*
+echo "Gotham Coding";
+echo "Gotham Coding";
+echo "Gotham Coding";
+echo "Gotham Coding";
+echo "Gotham Coding";
+echo "Gotham Coding";
+*/
+
+?>
