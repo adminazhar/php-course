19dd62c5f8bc66e8d0e7c4822358482ce9bd5777
Azhar Khan <admin@azhark.com>
2022-10-06 14:08:18 +0530

    Lecture 2

---
+++ 2variable.php
@@ -0,0 +1,34 @@
+<?php
+// Declaring and Assigning value to variable
+$firstname = "Bye World";
+
+// Declaring and Assigning value to variable
+$firstname = "Hello World";
+
+// Printing the variable in browser
+echo $firstname;
+
+echo "<br>";
+
+// Creating Number data type variable and assigning value
+$firstnumber = 123;
+
+// Printing the variable in browser
+echo $firstnumber;
+
+echo "<br>";
+
+$firstfloat = 500.125;
+echo $firstfloat;
+
+echo "<br><br>Difference between double quotes and single quote<br>";
+
+echo 'Hello"s World$firstnumber';
+
+
+echo "<br>";
+echo "Bye's $firstnumber World";
+
+
+
+?>
